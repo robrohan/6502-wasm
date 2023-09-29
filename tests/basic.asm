f9fb351122cbf25782d5655bd2c1977dbc5f0a7a
Rob Rohan <rob@therohans.com>
2023-09-29 22:52:51 +1300

    Playing with screen sizes

--- tests/basic.asm
+++ tests/basic.asm
@@ -1,4 +1,27 @@
 ;======================================
+; Cycle through a bunch of colours on
+; my made up display (which is 256x190
+; when breaking the bytes into halfs 
+; and using each half as a colour look
+; up. Not sure this would work in the
+; real world yet.
+;
+; This works by drawing columns of 
+; colours. It puts the Accumulator into
+; each section of screen by row. Then
+; the display doubles the row size by
+; drawing each half of the byte in a 
+; column format.
+;   y   $2000
+; x +------------------
+;   |    X1
+;   |    X2
+;   |      
+;   |     |
+;   |    \ /
+;   |
+; Where X1 is the low order bits and X2
+; is the high order bits
 ;
 ;======================================
 LDX #$06
@@ -28,7 +51,9 @@ start:
     STA $2D00, X
     STA $2E00, X
     STA $2F00, X
-    ; INX
+
+    ADC #$01
+
     STA $3000, X          ; offset
     STA $3100, X
     STA $3200, X
@@ -45,7 +70,9 @@ start:
     STA $3D00, X
     STA $3E00, X
     STA $3F00, X
-    ; INX
+
+    ADC #$01
+
     STA $4000, X          ; offset
     STA $4100, X
     STA $4200, X
@@ -62,7 +89,9 @@ start:
     STA $4D00, X
     STA $4E00, X
     STA $4F00, X
-    ; INX
+
+    ADC #$01
+
     STA $5000, X          ; offset
     STA $5100, X
     STA $5200, X
@@ -79,7 +108,9 @@ start:
     STA $5D00, X
     STA $5E00, X
     STA $5F00, X
-    ; INX
+
+    ADC #$01
+
     STA $6000, X          ; offset
     STA $6100, X
     STA $6200, X
@@ -89,30 +120,30 @@ start:
     STA $6600, X
     STA $6700, X
     STA $6800, X
-    STA $6900, X
-    STA $6A00, X
-    STA $6B00, X
-    STA $6C00, X
-    STA $6D00, X
-    STA $6E00, X
-    STA $6F00, X
+    ; STA $6900, X
+    ; STA $6A00, X
+    ; STA $6B00, X
+    ; STA $6C00, X
+    ; STA $6D00, X
+    ; STA $6E00, X
+    ; STA $6F00, X
     ; INX
-    STA $7000, X          ; offset
-    STA $7100, X
-    STA $7200, X
-    STA $7300, X
-    STA $7400, X
-    STA $7500, X
-    STA $7600, X
-    STA $7700, X
-    STA $7800, X
-    STA $7900, X
-    STA $7A00, X
-    STA $7B00, X
-    STA $7C00, X
-    STA $7D00, X
-    STA $7E00, X
-    STA $7F00, X
+    ; STA $7000, X          ; offset
+    ; STA $7100, X
+    ; STA $7200, X
+    ; STA $7300, X
+    ; STA $7400, X
+    ; STA $7500, X
+    ; STA $7600, X
+    ; STA $7700, X
+    ; STA $7800, X
+    ; STA $7900, X
+    ; STA $7A00, X
+    ; STA $7B00, X
+    ; STA $7C00, X
+    ; STA $7D00, X
+    ; STA $7E00, X
+    ; STA $7F00, X
     INX
     ADC #$01               ; cycle accumulator
     JMP start
